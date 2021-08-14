fb06c8b5e4efb836890c98605869bbb2a15e31a5
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2021-08-14 12:22:41 +0800

    did some refactoring -- separated some things into their own files

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
+const e = require("express");
+
 //on form submit
 $(function () {
   $(".new-person-form").on("submit", function (e) {
     e.preventDefault();
     var form = $(this);
     var formData = form.serialize();
-    console.log("ok");
+    //console.log("ok");
 
     $.ajax({
       url: "/person",
@@ -34,8 +36,41 @@ $(function () {
     $.ajax({
       url: "/person/" + personID,
       success: function (response) {
+        $("#content").html(response);
+      },
+    });
+  });
 
-        $('#content').html(response)
+  //updating records
+  $(".person-form").on("submit", function () {
+    e.preventDefault();
+    var form = $(this);
+  });
+
+  $(".person-form").on("submit", function (e) {
+    e.preventDefault();
+    var form = $(this);
+    var formData = form.serialize();
+    var personID = 1 // get person id here
+
+    //work in progress -- copied from new person form
+    $.ajax({
+      url: "/person/" + personID,
+      type: "POST",
+      data: formData,
+      success: function (response) {
+        var firstname = form.find('input[name="firstname"]').val();
+        var lastname = form.find('input[name="lastname"]').val();
+        var id = $("#content > div > form > div:nth-child(1) > input[type=hidden]").text();
+        console.log(id)
+
+        //TODO: instead of appending, find the id from the <tr> and update the row
+        var html = `<tr><td>${
+          parseInt(id) + 1
+        }</td> <td>${firstname}</td> <td>${lastname}<td></tr>`;
+
+        $("table tbody").append(html);
+        $("#msg").show().delay(3000).fadeOut();
       },
     });
   });
--- server.js
+++ server.js
@@ -1,52 +1,46 @@
 const express = require("express");
 const app = express();
-const { Pool } = require("pg");
+const db = require("./config/db");
 
-const db = new Pool({
-  user: "postgres",
-  host: "localhost",
-  port: "5432",
-  password: "qwe123",
-  database: "contacts",
-});
+app.use(express.urlencoded({ urlencoded: true }));
+//routes
+app.use("/", require("./routes/person"));
 
 app
 
-  .use(express.static("public"))
-
-  .use(express.urlencoded({ urlencoded: true }))
+  .use(express.static("public"))  
 
   .set("view engine", "ejs")
 
-  .get("/", async (req, res) => {
-    const result = await db.query("SELECT * FROM people");
-    const data = result.rows;
+  // .get("/", async (req, res) => {
+  //   const result = await db.query("SELECT * FROM people");
+  //   const data = result.rows;
 
-    res.render("index", { people: data });
-  })
+  //   res.render("index", { people: data });
+  // })
 
-  .get("/person/:id", async (req, res) => {
-    const { id } = req.params;
+  // .get("/person/:id", async (req, res) => {
+  //   const { id } = req.params;
 
-    const result = await db.query(
-      `SELECT id, firstname, lastname FROM people WHERE id=$1`,
-      [id]
-    );
+  //   const result = await db.query(
+  //     `SELECT id, firstname, lastname FROM people WHERE id=$1`,
+  //     [id]
+  //   );
 
-    const data = result.rows[0];
+  //   const data = result.rows[0];
 
-    res.render('person/form', data)
-  })
+  //   res.render("person/form", data);
+  // })
 
-  .post("/person", async (req, res) => {
-    const { firstname, lastname } = req.body;
-    const result = await db.query(
-      `INSERT INTO people (firstname, lastname) 
-      VALUES ($1,$2)`,
-      [firstname, lastname]
-    );
+  // .post("/person", async (req, res) => {
+  //   const { firstname, lastname } = req.body;
+  //   const result = await db.query(
+  //     `INSERT INTO people (firstname, lastname) 
+  //     VALUES ($1,$2)`,
+  //     [firstname, lastname]
+  //   );
 
-    res.status(200).end();
-  })
+  //   res.status(200).end();
+  // })
 
   .listen(5000, () => console.log(`Server Running`));
