bed22cdc84999795d22e2a82e9e7780bb8abc4f6
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2021-08-17 10:21:19 +0800

    created the routeservice file

--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-const e = require("express");
-
 //on form submit
 $(function () {
   $(".new-person-form").on("submit", function (e) {
@@ -47,31 +45,31 @@ $(function () {
     var form = $(this);
   });
 
-  $(".person-form").on("submit", function (e) {
-    e.preventDefault();
-    var form = $(this);
-    var formData = form.serialize();
-    var personID = 1 // get person id here
+  // $(".person-form").on("submit", function (e) {
+  //   e.preventDefault();
+  //   var form = $(this);
+  //   var formData = form.serialize();
+  //   var personID = 1 // get person id here
 
-    //work in progress -- copied from new person form
-    $.ajax({
-      url: "/person/" + personID,
-      type: "POST",
-      data: formData,
-      success: function (response) {
-        var firstname = form.find('input[name="firstname"]').val();
-        var lastname = form.find('input[name="lastname"]').val();
-        var id = $("#content > div > form > div:nth-child(1) > input[type=hidden]").text();
-        console.log(id)
+  //   //work in progress -- copied from new person form
+  //   $.ajax({
+  //     url: "/person/" + personID,
+  //     type: "POST",
+  //     data: formData,
+  //     success: function (response) {
+  //       var firstname = form.find('input[name="firstname"]').val();
+  //       var lastname = form.find('input[name="lastname"]').val();
+  //       var id = $("#content > div > form > div:nth-child(1) > input[type=hidden]").text();
+  //       console.log(id)
 
-        //TODO: instead of appending, find the id from the <tr> and update the row
-        var html = `<tr><td>${
-          parseInt(id) + 1
-        }</td> <td>${firstname}</td> <td>${lastname}<td></tr>`;
+  //       //TODO: instead of appending, find the id from the <tr> and update the row
+  //       var html = `<tr><td>${
+  //         parseInt(id) + 1
+  //       }</td> <td>${firstname}</td> <td>${lastname}<td></tr>`;
 
-        $("table tbody").append(html);
-        $("#msg").show().delay(3000).fadeOut();
-      },
-    });
-  });
+  //       $("table tbody").append(html);
+  //       $("#msg").show().delay(3000).fadeOut();
+  //     },
+  //   });
+  // });
 });
